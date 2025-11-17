b39ea1c3091b65c0b8d724a0647580c52d03401f
AC-dev16 <amy_mb@hotmail.co.uk>
2025-11-17 13:41:40 +0000

    Add tests for CollaborateForm and CommentForm, and update settings for test database

---
+++ about/test_views.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from django.urls import reverse
+from .models import About
+from .forms import CollaborateForm
+
+
+class TestAboutView(TestCase):
+
+    def setUp(self):
+        """Creates about me content"""
+        self.about_content = About(
+            title="About Me", content="This is about me.")
+        self.about_content.save()
+
+    def test_render_about_page_with_collaborate_form(self):
+        """Verifies get request for about me containing a collaboration form"""
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'About Me', response.content)
+        self.assertIsInstance(
+            response.context['collaborate_form'], CollaborateForm)
+        
+    def test_successful_collaborate_form_submission(self):
+        """Test for posting a collaboration request"""
+        post_data = {
+            'name': 'Test User',
+            'email': 'test@example.com',
+            'message': 'This is a test message.'
+        }
+        response = self.client.post(reverse('about'), post_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            b'Collaboration request received! I endeavour to respond within 2 working days.',
+            response.content
+        )
